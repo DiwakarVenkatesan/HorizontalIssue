2b6174e8355cc1217d784c3a69f3fdca43a93563
DiwakarVenkatesan <97871609+DiwakarVenkatesan@users.noreply.github.com>
2023-08-22 14:30:56 +0530

    Add files via upload

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void Button_OnClicked(object sender, EventArgs e)
+	{
+		leftPanel.IsVisible = !leftPanel.IsVisible;
+	}
+}
+
